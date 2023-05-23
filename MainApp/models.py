bcec94ed3293b0fa4a74ec7b9018d68b7b59e664
boo <eyurchenko@specialist.ru>
2023-05-24 00:33:05 +0700

    add comments in process...

--- MainApp/models.py
+++ MainApp/models.py
@@ -16,3 +16,10 @@ class Snippet(models.Model):
     user = models.ForeignKey(to=User, on_delete=models.CASCADE,
                              blank=True, null=True)
     private = models.BooleanField(default=True)
+
+
+class Comment(models.Model):
+    text = models.TextField(max_length=1000)
+    creation_date = models.DateTimeField(auto_now=True)
+    author = models.ForeignKey(to=User, on_delete=models.CASCADE)
+    snippet = models.ForeignKey(to=Snippet, on_delete=models.CASCADE, related_name='comments')
